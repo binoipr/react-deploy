d157fa488f6d94528d5adbd81615321b3b80bea3
Binoy P R <binoi.pr@gmail.com>
2023-01-25 11:19:40 +0530

    Login color change

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
     <>
       {!authenticated && (
         <>
-          <h1>Login</h1>
+          <h1 className="app-login">Login</h1>
           <button type="button" onClick={() => setAuthenticated(true)}>
             Click here to login
           </button>
